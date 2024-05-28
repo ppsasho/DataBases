4706cdae76199d4bc1b38c8925c9918e0d609699
Sasho <149786305+ppsasho@users.noreply.github.com>
2024-05-28 15:18:19 +0200

    Update Homework 02.1.sql

--- homework/class 06/Homework 02.1.sql
+++ homework/class 06/Homework 02.1.sql
@@ -1,9 +1,5 @@
 -- Create new procedure called CreateGrade;
 -- Procedure should create only Grade header info (not Grade Details);
--- Procedure should return the total number of grades in the system for
--- the Student on input (from the CreateGrade);
--- Procedure should return second resultset with the MAX Grade of all
--- grades for the Student and Teacher on input (regardless the Course).
 
 Create procedure dbo.sp_CreateGrade(
 	 @StudentId int
@@ -32,13 +28,16 @@ Begin
            ,@Comment
            ,@CreatedDate)
 
-		   
+-- Procedure should return the total number of grades in the system for
+-- the Student on input (from the CreateGrade);
 		   Select StudentID, s.[FirstName], s.LastName, Count(Grade) as TotalGrades
 		   From Grade as g
 		   Inner join Student as s on s.ID = @StudentId
 		   Where StudentID = 1
 		   group by StudentID, s.[FirstName], s.LastName
-		   
+
+-- Procedure should return second resultset with the MAX Grade of all
+-- grades for the Student and Teacher on input (regardless the Course).
 		   Select g.StudentID, s.FirstName, s.LastName, t.ID as TeacherID, t.FirstName as TeacherName, Max(Grade) as MaxGrade
 		   From Grade as g
 		   Inner join Teacher as t ON TeacherID = t.ID
